bd22f69c58acccdc96640b89d7f0eb41e12c2d6b
Vedant Pandey <130579621+vedant22p@users.noreply.github.com>
2023-05-13 08:27:01 +0530

    Create populating_next_right_pointer_in_each_node.cpp

---
+++ Day 8/populating_next_right_pointer_in_each_node.cpp
@@ -0,0 +1,24 @@
+// Link to the problem: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+
+// Solution:
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return nullptr;
+        queue<Node*> q;
+        q.push(root);        
+        while(size(q)) {
+            Node* rightNode = nullptr;                   
+            for(int i = size(q); i; i--) {                
+                auto cur = q.front(); q.pop();            
+                cur -> next = rightNode;                  
+                rightNode = cur;                          
+                if(cur -> right)                          
+                    q.push(cur -> right),          
+                    q.push(cur -> left);                  
+            }
+        }
+        return root;
+    }
+};
