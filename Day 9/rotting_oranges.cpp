8fe8ccf4ec4e6a10ce9b0e442faa6188dfd428fc
Vedant Pandey <130579621+vedant22p@users.noreply.github.com>
2023-05-14 07:52:41 +0530

    Create rotting_oranges.cpp

---
+++ Day 9/rotting_oranges.cpp
@@ -0,0 +1,50 @@
+// Link to the problem: https://leetcode.com/problems/rotting-oranges/
+
+
+// Solution:
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==2){
+                vis[i][j]=1;
+                q.push({{i,j},0});
+                }
+        }
+            
+        }
+        int rows[]={1,0,-1,0};
+        int cols[]={0,1,0,-1};
+        int ans=0;
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int t=q.front().second;
+            ans=max(ans,t);
+            q.pop();
+            for(int i=0; i<4; i++){
+                int nrow=row+rows[i];
+                int ncol=col+cols[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    vis[nrow][ncol]=1;
+                    grid[nrow][ncol]=2;
+                    q.push({{nrow,ncol},t+1});
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        return ans;
+
+    }
+};
