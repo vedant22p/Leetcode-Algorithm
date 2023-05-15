3513b483ac66410eb2d3aaefe92fbf1ed23cf700
Vedant Pandey <130579621+vedant22p@users.noreply.github.com>
2023-05-15 06:56:12 +0530

    Create reverse_linked_list.cpp

---
+++ Day 10/reverse_linked_list.cpp
@@ -0,0 +1,15 @@
+// Link to the problem: https://leetcode.com/problems/reverse-linked-list/
+
+
+// Solution:
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* prev = NULL;
+        ListNode* h2 = reverseList(head->next);
+        head->next->next = head;
+        head->next=prev;
+        return h2;
+    }
+};
