9552260a4ba124e92b472226d71e5681e8d2ebbd
Vedant Pandey <130579621+vedant22p@users.noreply.github.com>
2023-05-17 06:53:21 +0530

    Create house_robber.cpp

---
+++ Day 12/house_robber.cpp
@@ -0,0 +1,18 @@
+// Link to the problem: https://leetcode.com/problems/house-robber/
+
+
+// Solution:
+class Solution {
+ public:
+  int rob(vector<int>& nums) {
+    int prev1 = 0;
+    int prev2 = 0;
+
+    for (const int num : nums) {
+      const int dp = max(prev1, prev2 + num);
+      prev2 = prev1;
+      prev1 = dp;
+    }
+    return prev1;
+  }
+};
